c329469ee85f6a5c8c335794eba6355b03e20f71
Jaima Islam <r6379616@gmail.com>
2026-09-21 00:08:42 +0600

    Delete src directory

--- src/data/initialData.ts
+++
@@ -1,9 +0,0 @@
-import { PostItem, CommentItem, ChatMessage } from '../types';
-
-// All demo posts/comments have been deleted as requested.
-export const initialPosts: PostItem[] = [];
-
-export const initialComments: CommentItem[] = [];
-
-export const initialChatMessages: ChatMessage[] = [];
-
